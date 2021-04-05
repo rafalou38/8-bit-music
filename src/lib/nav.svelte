d5c9a88dad8292a08374d61c0f84c50089027e10
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-05 20:09:45 +0200

    initial comit

---
+++ src/lib/nav.svelte
@@ -0,0 +1,20 @@
+<header class="nav">
+	<h1 class="nav__title">8-bit-music</h1>
+</header>
+
+<style lang="scss">
+	@import './theme.scss';
+
+	.nav {
+		height: 56px;
+		padding: 16px;
+		&__title {
+			font-family: 'Rubik';
+			font-weight: 500;
+			font-size: 20px;
+			text-transform: capitalize;
+
+			background: $primary;
+		}
+	}
+</style>
---
+++ src/routes/$layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.scss';
+	import Nav from '$lib/nav.svelte';
+</script>
+
+<Nav />
+<slot />
