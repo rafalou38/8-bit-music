ec5c145c9bf5b4c1411c97bb6c56dc85b4d881b7
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-05 21:46:04 +0200

    imroved layout
    reorganised
    added sidebar

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,7 +1,25 @@
 <script>
 	import '../app.scss';
 	import Nav from '$lib/nav.svelte';
+	import Sidebar from '$lib/sidebar.svelte';
 </script>
 
-<Nav />
-<slot />
+<div class="body">
+	<Nav />
+	<div class="wrapper">
+		<Sidebar />
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	.body {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	.wrapper {
+		display: flex;
+		flex-grow: 1;
+	}
+</style>
