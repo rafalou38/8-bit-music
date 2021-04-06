d0a6fb013d8e608b30007773aa91221d469622e4
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-06 20:53:34 +0200

    improved SEO

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,8 +1,19 @@
 <script>
+	import { page } from '$app/stores';
+
 	import '../app.scss';
 	import Nav from '$lib/nav.svelte';
 	import Sidebar from '$lib/sidebar.svelte';
 </script>
+
+<svelte:head>
+	<title
+		>8-Bit-Music | {$page.path
+			.split('/')
+			.filter((e) => e)
+			.pop()}</title
+	>
+</svelte:head>
 
 <div class="body">
 	<Nav />
