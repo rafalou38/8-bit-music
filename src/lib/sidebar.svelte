d80502551e2b97c01beb26fe4439515ee21f5c98
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-06 12:15:00 +0200

    add active state to nav

--- src/lib/sidebar.svelte
+++ src/lib/sidebar.svelte
@@ -1,22 +1,38 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	function url(node: HTMLElement, path: string) {
+		node.setAttribute('href', path);
+		node.setAttribute('title', path);
+		page.subscribe((newPage) => {
+			if ('/' + path == newPage.path) {
+				node.classList.add('sidebar__link--active');
+			} else {
+				node.classList.remove('sidebar__link--active');
+			}
+		});
+	}
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
 <nav class="sidebar">
 	<ul class="sidebar__list">
 		<li class="sidebar__element">
-			<a class="sidebar__link" href="/editor"
+			<a class="sidebar__link sidebar__link--active" use:url={'editor'}
 				><span class="iconify" data-icon="mdi:playlist-music" /></a
 			>
 		</li>
 		<li class="sidebar__element">
-			<a class="sidebar__link" href="/export"
+			<a class="sidebar__link" use:url={'export'}
 				><span class="iconify" data-icon="mdi:export-variant" /></a
 			>
 		</li>
 		<li class="sidebar__element">
-			<a class="sidebar__link" href="/comunity"
+			<a class="sidebar__link" use:url={'comunity'}
 				><span class="iconify" data-icon="mdi:cloud-search" /></a
 			>
 		</li>
 		<li class="sidebar__element">
-			<a class="sidebar__link" href="/settings"><span class="iconify" data-icon="mdi:cog" /></a>
+			<a class="sidebar__link" use:url={'settings'}><span class="iconify" data-icon="mdi:cog" /></a>
 		</li>
 	</ul>
 </nav>
@@ -47,7 +63,7 @@
 			place-items: center;
 
 			font-size: 200%;
-
+			&--active,
 			&:hover {
 				color: white;
 			}
