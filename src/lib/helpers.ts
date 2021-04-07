90432c874415567ddab8d66e747097d37a65a5d4
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-07 15:00:45 +0200

    added map helper

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,12 @@
 export function uniqueID(): string {
 	return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase();
 }
+export function map(
+	x: number,
+	in_min: number,
+	in_max: number,
+	out_min: number,
+	out_max: number
+): number {
+	return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
