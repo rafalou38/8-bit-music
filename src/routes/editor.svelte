31a72ffc0452b4aacdb8ac23d011f6e50530ef34
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-09 13:33:19 +0200

    added play/pause/loop fonctionality

--- src/routes/editor.svelte
+++ src/routes/editor.svelte
@@ -1,46 +1,26 @@
 <script lang="ts">
 	import Board from '$lib/editor/board.svelte';
-	import ToolBar from '$lib/editor/topBar.svelte';
+	import ToolBar from '$lib/editor/toolBar.svelte';
 	import { map, uniqueID } from '$lib/helpers';
+	import { onDestroy, onMount } from 'svelte';
 
-	let keys_count = 30;
+	import * as Tone from 'tone';
+	let synth: Tone.PolySynth<Tone.Synth<Tone.SynthOptions>>;
+	onMount(() => {
+		if (!synth) {
+			synth = new Tone.PolySynth().toDestination();
+		}
+	});
+
+	let keys_count = 5;
 	let multiple_alowed = false;
-	let rawNotes = [
-		'C1',
-		'D1',
-		'E1',
-		'F1',
-		'G1',
-		'A1',
-		'B1',
-		'C2',
-		'D2',
-		'E2',
-		'F2',
-		'G2',
-		'A2',
-		'B2',
-		'C3',
-		'D3',
-		'E3',
-		'F3',
-		'G3',
-		'A3',
-		'B3',
-		'C4',
-		'D4',
-		'E4',
-		'F4',
-		'G4',
-		'A4',
-		'B4',
-		'C5',
-		'D5',
-		'E5',
-		'F5',
-		'G5',
-		'A5'
-	];
+	let current_row = -1;
+	let playing: boolean;
+	let looping: boolean;
+
+	let interval: NodeJS.Timeout;
+
+	let rawNotes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4'];
 	let notes: INote[] = [];
 	for (let i = 0; i < rawNotes.length; i++) {
 		const note = rawNotes[i];
@@ -54,16 +34,66 @@
 	}
 
 	let EToolBar: ToolBar;
+
+	onDestroy(() => {
+		playing = false;
+	});
+
+	let last_time = 0;
+	let i = 0;
+	function loop() {
+		if (playing) {
+			let target = 0.3;
+
+			if (i >= keys_count) {
+				if (!looping) {
+					playing = false;
+					current_row = -1;
+				}
+				i = 0;
+			}
+
+			if ((Date.now() - last_time) / 1000 >= target) {
+				let next = [];
+				current_row = i % keys_count;
+				notes.forEach((note, i) => {
+					let key = note.keys[current_row].active;
+					if (key) {
+						next.push(note.note);
+					}
+				});
+				synth.triggerAttackRelease(next, 0.2);
+				last_time = Date.now();
+				i += 1;
+			}
+		}
+	}
+	interval = setInterval(loop, 100);
+	async function play() {
+		// debugger;
+		// if (playing) {
+		// 	i = 0;
+		// 	last_time = 0;
+		// }
+	}
+
 	function handleAction(action: 'add' | 'delete' | 'settings' | 'clock' | 'play' | 'pause') {
-		console.log({ action, playing: EToolBar.playing, looping: EToolBar.looping });
+		switch (action) {
+			case 'play':
+				play();
+				break;
+
+			default:
+				break;
+		}
 	}
 </script>
 
 <div class="wrapper">
-	<ToolBar onAction={handleAction} bind:this={EToolBar} />
+	<ToolBar onAction={handleAction} bind:this={EToolBar} bind:looping bind:playing />
 	<input type="checkbox" bind:checked={multiple_alowed} />
-
-	<Board {notes} {keys_count} {multiple_alowed} />
+	<pre>{keys_count}</pre>
+	<Board bind:notes bind:keys_count {multiple_alowed} {current_row} />
 </div>
 
 <style lang="scss">
