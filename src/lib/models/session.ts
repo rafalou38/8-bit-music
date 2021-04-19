e02d4f056b7fd5b29361f07717c69a619cceae93
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-19 19:02:59 +0200

    added session model

---
+++ src/lib/models/session.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export interface IUserSession extends mongoose.Document {
+  username: string;
+
+	uuid: string;
+}
+
+export const UserSchema = new mongoose.Schema({
+  username: String,
+	uuid: String
+}, {timestamps:true});
+
+
+UserSchema.index({createdAt: 1},{expireAfterSeconds: 60 * 60 * 24 * 7});
+const UserSession = mongoose.model<IUserSession>("Session", UserSchema);
+export default UserSession;
---
+++ src/lib/server/sessions/index.ts
@@ -0,0 +1,14 @@
+import UserSession from "$lib/models/session";
+import type { IUser } from "$lib/models/user";
+import { v4 as uuidv4 } from "uuid";
+
+export async function createSession(user: IUser) {
+	await UserSession.deleteMany({
+		username: user.username,
+	}).exec();
+
+	const uuid: string = uuidv4();
+	await UserSession.create({ username: user.username, uuid });
+
+	return uuid;
+}
