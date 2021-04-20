322385e8717be36f1d4ff667eacc2cdced9006ce
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-20 17:21:23 +0200

    fixed endless build

--- src/lib/server/mongoose.ts
+++ src/lib/server/mongoose.ts
@@ -1,21 +1,17 @@
-import mongoose from "mongoose";
-import { MONGO_URI } from "../keys";
+import mongoose from 'mongoose';
+import { MONGO_URI } from '../keys';
 
-mongoose.set("useFindAndModify", false);
-
+mongoose.set('useFindAndModify', false);
 
 export const connect = new Promise<void>((resolve, reject) => {
-  mongoose.connect(
-    MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (err) {
-        console.error("there was an error connecting to MongoDB ❌");
-        console.error(err);
-        return reject();
-      }
-      console.log("connected to MongoDB ✅");
-      return resolve();
-    }
-  );
+	if (process.env['npm_lifecycle_event'] === 'build') return;
+	mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+		if (err) {
+			console.error('there was an error connecting to MongoDB ❌');
+			console.error(err);
+			return reject();
+		}
+		console.log('connected to MongoDB ✅');
+		return resolve();
+	});
 });
