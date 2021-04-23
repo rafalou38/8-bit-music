6fb148c23b9c1dff964a3e29937c2db38563197e
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-23 19:32:23 +0200

    added option to logout + improved profile styles

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,12 +1,74 @@
 <script lang="ts">
+	import { Button, Icon } from 'svelte-materialify';
+	import { mdiLogoutVariant } from '@mdi/js';
+	import jsCookies from 'js-cookie';
+
 	import { goto } from '$app/navigation';
 
 	import { sessionStore } from '$lib/stores';
+	import { browser } from '$app/env';
 
-	$: if ($sessionStore.authenticated) {
-		goto('/profile');
+	$: if (!$sessionStore.authenticated && browser) {
+		goto('/login');
+	}
+
+	function logout() {
+		jsCookies.remove('session');
+		sessionStore.set({ authenticated: false });
+		goto('/');
 	}
 </script>
 
-<p>you are {$sessionStore.username}</p>
+<div class="wrapper">
+	<div class="main">
+		<h2>Profile</h2>
+		<h3>you are <b>{$sessionStore.username}</b></h3>
+		<Button class="logout-btn" on:click={logout}
+			>Logout <Icon path={mdiLogoutVariant} class="ml-3" /></Button
+		>
+	</div>
+</div>
+
 <slot />
+
+<style lang="scss">
+	@use "../theme.scss";
+	@use "../mixins.scss";
+	h2 {
+		font-size: 2.25rem;
+		font-weight: 500;
+	}
+	h3 {
+		font-size: 1.5rem;
+		font-weight: 400;
+		margin-bottom: 1em;
+	}
+	.wrapper {
+		flex-grow: 1;
+		width: 100%;
+		background: whitesmoke;
+	}
+	.main {
+		@include mixins.elevation;
+
+		position: relative;
+
+		width: clamp(400px, 60%, 600px);
+		height: 100%;
+
+		margin: 0 auto;
+		padding: 1em;
+
+		font-family: 'Roboto';
+		background: #fafafa;
+
+		display: flex;
+
+		flex-direction: column;
+		align-items: center;
+	}
+	:global(.logout-btn) {
+		background: theme.$primary;
+		color: white;
+	}
+</style>
