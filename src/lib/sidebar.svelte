a618158077e108b7dcabe48af5faee8d3e226da1
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-23 19:29:06 +0200

    changed so that username overflows of profile and added username title

--- src/lib/sidebar.svelte
+++ src/lib/sidebar.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { sessionStore } from './stores';
-	function url(node: HTMLElement, path: string) {
+	function url(node: HTMLElement, { path, title }: { path: string; title?: string }) {
 		node.setAttribute('href', path);
-		node.setAttribute('title', path);
+		node.setAttribute('title', title ? title : path);
 		page.subscribe((newPage) => {
 			if ('/' + path == newPage.path) {
 				node.classList.add('sidebar__link--active');
@@ -18,27 +18,32 @@
 <nav class="sidebar">
 	<ul class="sidebar__list">
 		<li class="sidebar__element">
-			<a class="sidebar__link sidebar__link--active" use:url={'editor'}
+			<a class="sidebar__link sidebar__link--active" use:url={{ path: 'editor' }}
 				><span class="iconify" data-icon="mdi:playlist-music" /></a
 			>
 		</li>
 		<li class="sidebar__element">
-			<a class="sidebar__link" use:url={'export'}
+			<a class="sidebar__link" use:url={{ path: 'export' }}
 				><span class="iconify" data-icon="mdi:export-variant" /></a
 			>
 		</li>
 		<li class="sidebar__element">
-			<a class="sidebar__link" use:url={'comunity'}
+			<a class="sidebar__link" use:url={{ path: 'comunity' }}
 				><span class="iconify" data-icon="mdi:cloud-search" /></a
 			>
 		</li>
 		<li class="sidebar__element">
-			<a class="sidebar__link" use:url={'settings'}><span class="iconify" data-icon="mdi:cog" /></a>
+			<a class="sidebar__link" use:url={{ path: 'settings' }}
+				><span class="iconify" data-icon="mdi:cog" /></a
+			>
 		</li>
 	</ul>
 	<div class="sidebar__auth">
 		{#if $sessionStore.authenticated}
-			<a class="sidebar__element sidebar__link--active" use:url={'profile'}>
+			<a
+				class="sidebar__element sidebar__link--active"
+				use:url={{ path: 'profile', title: $sessionStore.username }}
+			>
 				<span class="iconify account-btn" data-icon="mdi:account" />
 				<span class="account-label">{$sessionStore.username}</span>
 			</a>
@@ -121,8 +126,8 @@
 					display: block;
 					margin: 0 auto;
 					width: 80%;
-					overflow-wrap: break-word;
 					line-height: 1.5ch;
+					overflow: hidden;
 				}
 				&:hover {
 					color: theme.$sidebar-icons--hover;
