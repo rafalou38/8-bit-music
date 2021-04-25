ab44d012d865d155aec7c25df90f9e1e8fbd5682
Rafael <rafael.martinezcalvo@orange.fr>
2021-04-25 15:27:13 +0200

    added svelte dnd

--- src/types/editor.d.ts
+++ src/types/editor.d.ts
@@ -10,3 +10,11 @@ declare interface INote {
 	color: string;
 	keys: IKey[];
 }
+
+declare type DndEvent = import('svelte-dnd-action').DndEvent;
+declare namespace svelte.JSX {
+	interface HTMLAttributes<T> {
+		onconsider?: (event: CustomEvent<DndEvent> & { target: EventTarget & T }) => void;
+		onfinalize?: (event: CustomEvent<DndEvent> & { target: EventTarget & T }) => void;
+	}
+}
