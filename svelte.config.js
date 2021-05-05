7a164b21497267c7da466999acafbd1854262edc
Rafael <rafael.martinezcalvo@orange.fr>
2021-05-05 14:08:53 +0200

    setup new svelte-kit version

---
+++ svelte.config.js
@@ -0,0 +1,35 @@
+import preprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess({
+		scss: {
+			includePaths: ['theme']
+		}
+	}),
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: node(),
+		vite: {
+			ssr: {
+				noExternal: [
+					'@smui/button',
+					'@smui/fab',
+					'@smui/icon-button',
+					'@smui/list',
+					'@smui/paper',
+					'@smui/ripple',
+					'@smui/select',
+					'@smui/textfield'
+				]
+			}
+		}
+	}
+};
+
+export default config;
