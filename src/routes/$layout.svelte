cd366b1a4b6861088162d00443b72296e06af32c
Rafael <rafael.martinezcalvo@orange.fr>
2021-08-03 13:57:34 +0200

    add buttons for create or import

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Fab, { Icon } from '@smui/fab';
+	import Button, { Label } from '@smui/button';
 	import Svg from '@smui/common/Svg.svelte';
 	import { page } from '$app/stores';
 
@@ -27,11 +28,33 @@
 		</main>
 	</div>
 </div>
-<a href="/editor/new" class="add-fab" title="create a new song">
-	<Fab color="primary" title="create a new song">
-		<Icon component={Svg} viewBox="4 4 16 16"><path fill="currentColor" d={mdiPlus} /></Icon>
-	</Fab>
-</a>
+<div class="add">
+	<a href="/editor/import" title="create a new song">
+		<Button variant="raised" class="button-shaped-round">
+			<Icon class="material-icons">file_upload</Icon>
+			<Label>Import</Label>
+		</Button>
+		<!-- <Fab extended color="primary">
+			<Icon class="material-icons">file_upload</Icon>
+			<Label>Import</Label>
+		</Fab> -->
+	</a>
+	<a href="/editor/new" title="create a new song">
+		<Button variant="raised" class="button-shaped-round">
+			<Icon class="material-icons">create</Icon>
+			<Label>Create</Label>
+		</Button>
+		<!-- <Fab extended color="primary">
+			<Icon class="material-icons">create</Icon>
+			<Label>Create</Label>
+		</Fab> -->
+	</a>
+	<div class="add-fab">
+		<Fab color="primary" title="create a new song">
+			<Icon component={Svg} viewBox="4 4 16 16"><path fill="currentColor" d={mdiPlus} /></Icon>
+		</Fab>
+	</div>
+</div>
 
 <style lang="scss">
 	.body {
@@ -49,9 +72,45 @@
 
 		position: relative;
 	}
-	.add-fab {
+	.add {
 		position: fixed;
 		right: 16px;
 		bottom: 16px;
+		text-align: right;
+		&:hover {
+			.add-fab {
+				display: none;
+			}
+			& > *:not(.add-fab) {
+				// &:first-child {
+				// 	background: red;
+				// }
+				// &:last-child {
+				// 	background: green;
+				// }
+				&:nth-child(1) {
+					animation: slide-top 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0ms forwards;
+				}
+				&:nth-child(2) {
+					animation: slide-top 300ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 100ms forwards;
+				}
+			}
+		}
+		& > *:not(.add-fab) {
+			margin-top: 0.5em;
+			opacity: 0;
+			display: block;
+		}
+	}
+
+	@keyframes slide-top {
+		0% {
+			transform: translateY(20px);
+			opacity: 0;
+		}
+		100% {
+			transform: translateY(0);
+			opacity: 1;
+		}
 	}
 </style>
